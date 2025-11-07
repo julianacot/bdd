8076a1def206a6af282a544976631f8d0aa92f90
Rodrigo Victor Beltrão <150438774+Rodibre8708@users.noreply.github.com>
2025-11-07 19:45:22 -0300

    Add new user 'cadastrador' with permissions

--- banco/usuarios.sql
+++ banco/usuarios.sql
@@ -7,4 +7,8 @@ GRANT SELECT, DELETE, UPDATE ON supermais.* TO 'gerente'@'localhost';
 CREATE USER 'funcionario'@'localhost' IDENTIFIED BY 'func123';
 GRANT SELECT, INSERT ON supermais.vendas TO 'funcionario'@'localhost';
 
+CREATE USER 'cadastrador'@'localhost' IDENTIFIED BY 'cad123';
+GRANT INSERT ON supermais.Cliente TO 'cadastrador'@'localhost';
+GRANT INSERT ON supermais.Produto TO 'cadastrador'@'localhost';
+
 FLUSH PRIVILEGES;
